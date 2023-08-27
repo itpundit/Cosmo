6c3fb5bf6d36d5c030f7ca2aaec14f34f3db0f68
SachinCosmo <mecosmo369@gmail.com>
2023-08-28 00:00:35 +0530

    this is commit

--- gittest.py
+++ gittest.py
@@ -1,1 +1,38 @@
-"This is git push"
+import pyttsx3
+import speech_recognition as sr
+import pyaudio
+import datetime
+import os
+import random
+import wikipedia
+import webbrowser
+import pywhatkit as kit
+
+engine=pyttsx3.init('sapi5')
+voices=engine.getProperty('voices')
+engine.setProperty('voices',voices[0].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def wish():
+
+    hour=int(datetime.datetime.now().hour)
+    if hour>0 and hour<12:
+        speak('Good Morning Sir')
+    elif hour>12 and hour<18:
+        speak('Good Afternoon Sir')
+    else :
+        speak('Good Evening Sir')
+
+    speak('How may I Help you?')
+    
+    
+    
+    
+    
+    
+    
+    
+    
